e0cc55622682a411f1d0cdc84e8e2f37b22bf7dd
escarface <axierlu@gmail.com>
2021-02-14 01:15:55 +0100

    app final

--- app.py
+++ app.py
@@ -5,19 +5,17 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
+
 def get_beer():
     r = requests.get('https://api.punkapi.com/v2/beers/random')
     beerjson = r.json()
+    
     beer = {
         'name': beerjson[0]['name'],
         'abv': beerjson[0]['abv'],
         'description': beerjson[0]['description'],
         'foodpair' : beerjson[0]['food_pairing'][0]
     }
-    print(beer)
-    
-    
-    
     return render_template('index.html', beer=beer)
 
 if __name__ == '__main__':
